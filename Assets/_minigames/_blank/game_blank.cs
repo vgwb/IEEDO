868ba0a107f444d4422813a2d17ea27e6000974e
Stefano Cecere <stefano.cecere@krur.com>
2022-01-19 16:25:20 +0100

    minigames: blank

---
+++ Assets/_minigames/_blank/game_blank.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ieedo.games
+{
+    public class game_blank : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void OnBtnWin()
+        {
+            Debug.Log("Game Blank Win");
+
+        }
+
+        public void OnBtnLose()
+        {
+            Debug.Log("Game Blank Lose");
+        }
+    }
+}
