14ed6076a3fdf57493b6613109efad40e2b9ab68
Michele <cat.piro@gmail.com>
2022-02-09 11:25:09 +0100

    Separated assessment questions into multiple folders. Added localized answers

--- Assets/_core/_scripts/core/Data/AssessmentQuestionDefinition.cs
+++ Assets/_core/_scripts/core/Data/AssessmentQuestionDefinition.cs
@@ -7,8 +7,14 @@ namespace Ieedo
     {
         public int ID;
         public LocalizedString Question;
-        public LocalizedString Category;
+        public CategoryID Category;
         [Range(0,2)]
         public float Weight = 1f;
+
+        public LocalizedString[] Answers = new LocalizedString[]
+        {
+            new LocalizedString() { DefaultText = "Yes" },
+            new LocalizedString() { DefaultText = "No" }
+        };
     }
 }
