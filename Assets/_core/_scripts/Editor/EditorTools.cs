04a2e99aac04f965ae3c29298c1e6b3db67a7f20
Stefano Cecere <stefano.cecere@krur.com>
2022-02-16 16:40:32 +0100

    Editor utility: reveal data path

---
+++ Assets/_core/_scripts/Editor/EditorTools.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+
+public class EditorTools : ScriptableObject
+{
+    [MenuItem("Tools/IEEDO/Revel Data Path")]
+    static void RevelDataPath()
+    {
+        //        EditorUtility.DisplayDialog("IEEDO", "Application Path: " + Application.persistentDataPath, "OK", "");
+        // Debug.Log("persistentDataPath: " + Application.persistentDataPath);
+        EditorUtility.RevealInFinder(Application.persistentDataPath);
+    }
+}
