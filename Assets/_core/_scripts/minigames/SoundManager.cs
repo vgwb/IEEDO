8a7a8728a839b1fd11d014b52b0284dfd2cbe1f1
Stefano Cecere <stefano.cecere@krur.com>
2022-03-02 17:01:59 +0100

    init Activity Commons

---
+++ Assets/_core/_scripts/minigames/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ieedo.games
+{
+    public enum sfx
+    {
+        click,
+        win,
+        lose
+    }
+
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioClip Click;
+
+    }
+}
