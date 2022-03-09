aa444d0fabf87ea5445fd1a4a5c8aee89f92e44c
Michele <cat.piro@gmail.com>
2022-03-09 12:08:55 +0100

    Added tooltip

---
+++ Assets/_core/_scripts/core/managers/Screens/UITooltip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ieedo
+{
+    public class UITooltip : MonoBehaviour
+    {
+        public Image BG;
+        public Image Arrow;
+        public UIText Text;
+    }
+}
