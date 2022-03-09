cdc79ee0fa2c0f6b26e7caf9010300e9df5e209e
Michele <cat.piro@gmail.com>
2022-03-09 16:51:43 +0100

    Added safe area checker to avoid notches.

---
+++ Assets/_core/_scripts/core/managers/Screens/SafeAreaChecker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ieedo
+{
+    public class SafeAreaChecker : MonoBehaviour
+    {
+        private CanvasScaler canvasScaler;
+        private float bottomUnits, topUnits;
+
+        void Start()
+        {
+            canvasScaler = FindObjectOfType<CanvasScaler>();
+            ApplyVerticalSafeArea();
+        }
+
+
+        public void ApplyVerticalSafeArea()
+        {
+            var bottomPixels = Screen.safeArea.y;
+            var topPixel = Screen.currentResolution.height - (Screen.safeArea.y + Screen.safeArea.height);
+
+            var bottomRatio = bottomPixels / Screen.currentResolution.height;
+            var topRatio = topPixel / Screen.currentResolution.height;
+
+            var referenceResolution = canvasScaler.referenceResolution;
+            bottomUnits = referenceResolution.y * bottomRatio;
+            topUnits = referenceResolution.y * topRatio;
+
+            var rectTransform = GetComponent<RectTransform>();
+            rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, bottomUnits);
+            rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -topUnits);
+        }
+    }
+}
