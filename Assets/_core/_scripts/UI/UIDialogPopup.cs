c0d85b4f9ed42c85c4f770aef3560785ded80cc9
Michele <cat.piro@gmail.com>
2022-03-30 11:22:25 +0200

    Added dialog popup

---
+++ Assets/_core/_scripts/UI/UIDialogPopup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.Localization;
+
+namespace Ieedo
+{
+    public class UIDialogPopup : UIScreen
+    {
+        public override ScreenID ID => ScreenID.Dialog;
+
+        public UITextContent Content;
+        public UIButton Button;
+
+        public IEnumerator ShowDialog(LocalizedString content, LocalizedString answer)
+        {
+            Content.Text.Key = content;
+            Button.Key = answer;
+
+            SetupButton(Button, Close);
+            yield return OpenCO();
+        }
+    }
+}
