2fb8bdede7038247147a0b408fd656defffd6744
Stefano Cecere <stefano.cecere@krur.com>
2022-03-31 00:40:45 +0200

    improve Activity Diary

--- Assets/_minigames/activity_diary/Scripts/ActivityDiary.cs
+++ Assets/_minigames/activity_diary/Scripts/ActivityDiary.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 namespace Ieedo.games.diary
 {
@@ -14,7 +16,13 @@ namespace Ieedo.games.diary
 
     public class ActivityDiary : ActivityManager
     {
+        public TMP_Text DateText;
+        public TMP_InputField InputText;
+        public GameObject BtnNext;
+        public GameObject BtnPrev;
+
         public List<Page> Pages = new List<Page>();
+        private int currentPageNumber;
 
         protected override void SetupActivity(int currentLevel)
         {
@@ -23,23 +31,69 @@ namespace Ieedo.games.diary
             var activityData = Statics.Data.Profile.ActivitiesData.GetActivityData(ActivityID.Diary);
 
             Pages.Clear();
-            foreach (ActivityResult activityResult in  activityData.Results)
+            if (activityData.Results.Count > 0)
+            {
+                foreach (ActivityResult activityResult in activityData.Results)
+                {
+                    Pages.Add(new Page
+                    {
+                        Text = activityResult.CustomData,
+                        Date = activityResult.Timestamp.Date,
+                    });
+                }
+            }
+            else
             {
                 Pages.Add(new Page
                 {
-                    Text = activityResult.CustomData,
-                    Date = activityResult.Timestamp.Date,
+                    Text = "write",
+                    Date = DateTime.Today,
                 });
             }
 
-            foreach (Page page in Pages)
+            // foreach (Page page in Pages)
+            // {
+            //     Debug.LogError(page.Date + ": " + page.Text);
+            // }
+            currentPageNumber = 1;
+            updateUI();
+        }
+
+        private void updateUI()
+        {
+            BtnNext.SetActive(Pages.Count > 1 && currentPageNumber < Pages.Count);
+            BtnPrev.SetActive(Pages.Count > 1 && currentPageNumber > 1);
+
+            DateText.text = Pages[currentPageNumber - 1].Date.ToString("dd-MM-yyyy");
+            InputText.text = Pages[currentPageNumber - 1].Text;
+        }
+
+        public void OnBtnDone()
+        {
+            CompleteActivity();
+        }
+
+        public void OnBtnNext()
+        {
+            if (currentPageNumber > 1)
             {
-                Debug.LogError(page.Date + ": " + page.Text);
+                currentPageNumber--;
+                updateUI();
             }
         }
 
-        public void OnComplete(string todayText)
+        public void OnBtnPrev()
         {
+            if (currentPageNumber < Pages.Count)
+            {
+                currentPageNumber++;
+                updateUI();
+            }
+        }
+
+        public void CompleteActivity()
+        {
+            var todayText = InputText.text;
             Debug.LogError("Updating page of " + Timestamp.Today.Date + " with text " + todayText);
             var activityData = Statics.Data.Profile.ActivitiesData.GetActivityData(ActivityID.Diary);
             var todayResult = activityData.Results.FirstOrDefault(x => x.Timestamp.Equals(Timestamp.Today));
