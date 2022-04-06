a726a486d8188d0e31f1cfb2102b4544d6ad9ed8
Michele <cat.piro@gmail.com>
2022-04-06 15:30:58 +0200

    Added ArabicSupport. Added UIText3D Added auto alignment reversal

--- Assets/_core/_scripts/UI/UIText.cs
+++ Assets/_core/_scripts/UI/UIText.cs
@@ -1,16 +1,83 @@
-﻿using UnityEngine;
+﻿using ArabicSupport;
+using UnityEngine;
 using TMPro;
 using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
 
 namespace Ieedo
 {
     public class UIText : TextMeshProUGUI
     {
+        public bool IgnoreRTL;
+
         public LocalizeStringEvent LocalizeStringEvent => GetComponent<LocalizeStringEvent>();
         public UnityEngine.Localization.LocalizedString Key
         {
             get => LocalizeStringEvent.StringReference;
             set => LocalizeStringEvent.StringReference = value;
+        }
+
+        public void SetTextRaw(string s)
+        {
+            IgnoreRTL = true;
+            Text = s;
+        }
+
+        public string Text
+        {
+            set
+            {
+                if (!IgnoreRTL && LocalizationSettings.Instance.GetSelectedLocale().Identifier.Code == "ar")
+                {
+                    alignment = RighterizeAlignment(alignment);
+                    text = ArabicFixer.Fix(value);
+                }
+                else
+                {
+                    if (!IgnoreRTL) alignment = LeftizeAlignment(alignment);
+                    text = value;
+                }
+            }
+        }
+
+        public static TextAlignmentOptions LeftizeAlignment(TextAlignmentOptions alignment)
+        {
+            switch (alignment)
+            {
+                case TextAlignmentOptions.Right:
+                    return TextAlignmentOptions.Left;
+                case TextAlignmentOptions.BaselineRight:
+                    return TextAlignmentOptions.BaselineLeft;
+                case TextAlignmentOptions.BottomRight:
+                    return TextAlignmentOptions.BottomLeft;
+                case TextAlignmentOptions.CaplineRight:
+                    return TextAlignmentOptions.CaplineLeft;
+                case TextAlignmentOptions.MidlineRight:
+                    return TextAlignmentOptions.MidlineLeft;
+                case TextAlignmentOptions.TopRight:
+                    return TextAlignmentOptions.TopLeft;
+            }
+            return alignment;
+        }
+
+        public static TextAlignmentOptions RighterizeAlignment(TextAlignmentOptions alignment)
+        {
+            switch (alignment)
+            {
+                case TextAlignmentOptions.Left:
+                    return TextAlignmentOptions.Right;
+                case TextAlignmentOptions.BaselineLeft:
+                    return TextAlignmentOptions.BaselineRight;
+                case TextAlignmentOptions.BottomLeft:
+                    return TextAlignmentOptions.BottomRight;
+                case TextAlignmentOptions.CaplineLeft:
+                    return TextAlignmentOptions.CaplineRight;
+                case TextAlignmentOptions.MidlineLeft:
+                    return TextAlignmentOptions.MidlineRight;
+                case TextAlignmentOptions.TopLeft:
+                    return TextAlignmentOptions.TopRight;
+            }
+            return alignment;
         }
     }
 }
---
+++ Assets/_core/_scripts/UI/UIText3D.cs
@@ -0,0 +1,41 @@
+﻿using ArabicSupport;
+using TMPro;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
+
+namespace Ieedo
+{
+    public class UIText3D : TextMeshPro
+    {
+        public bool IgnoreRTL;
+
+        public LocalizeStringEvent LocalizeStringEvent => GetComponent<LocalizeStringEvent>();
+        public UnityEngine.Localization.LocalizedString Key
+        {
+            get => LocalizeStringEvent.StringReference;
+            set => LocalizeStringEvent.StringReference = value;
+        }
+
+        public void SetTextRaw(string s)
+        {
+            IgnoreRTL = true;
+            Text = s;
+        }
+
+        public string Text
+        {
+            set
+            {
+                if (!IgnoreRTL && LocalizationSettings.Instance.GetSelectedLocale().Identifier.Code == "ar")
+                {
+                    text = ArabicFixer.Fix(value);
+                }
+                else
+                {
+                    text = value;
+                }
+            }
+        }
+
+    }
+}
