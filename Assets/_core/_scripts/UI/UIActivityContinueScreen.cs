4b6a0db022287001ea84bb81fb7c87cd95a4dde8
Michele <cat.piro@gmail.com>
2022-04-13 15:42:50 +0200

    Improved activity continue screen

--- Assets/_core/_scripts/UI/UIActivityContinueScreen.cs
+++ Assets/_core/_scripts/UI/UIActivityContinueScreen.cs
@@ -9,21 +9,30 @@ namespace Ieedo
         public override ScreenID ID => ScreenID.ActivityContinue;
 
         public UITextContent Title;
-        public UITextContent Result;
-        public UITextContent Score;
+        public UITextContent Content;
 
-        public UIButton ContinueButton;
+        public UIButton NextLevelButton;
         public UIButton QuitButton;
 
         public IEnumerator ShowResult(ActivityResult result)
         {
-            Title.Text.Key = new LocalizedString("UI", "activity_result_title");
-            Result.Text.Key = new LocalizedString("UI", $"activity_result_{result.Result.ToString().ToLower()}");
-            var locString = new LocalizedString("UI", "activity_result_score");
-            locString.Arguments = new List<object>{result};
-            Score.Text.Key = locString;
+            //Title.Text.Key = new LocalizedString("UI", "activity_result_title");
 
-            SetupButton(ContinueButton, Continue);
+            switch (Statics.ActivityFlow.CurrentActivity.Type)
+            {
+                case ActivityType.Daily:
+                    Title.Text.Key = new LocalizedString("UI", $"activity_result_daily_complete");
+                    Content.Text.Key =  new LocalizedString("UI", "activity_result_write_everyday");
+                    break;
+                case ActivityType.Game:
+                    Title.Text.Key = new LocalizedString("UI", $"activity_result_game_{result.Result.ToString().ToLower()}");
+                    Content.Text.Key =  new LocalizedString("UI", "activity_result_play_everyday");
+                    break;
+            }
+
+            NextLevelButton.gameObject.SetActive(Statics.ActivityFlow.CurrentActivity.Type == ActivityType.Game);
+
+            SetupButton(NextLevelButton, Continue);
             SetupButton(QuitButton, Quit);
             yield return OpenCO();
         }
