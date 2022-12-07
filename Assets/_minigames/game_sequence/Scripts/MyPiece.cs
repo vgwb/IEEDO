37aa305bc70c4f9811987df5e5dc0d2e713e39dc
Stefano Cecere <stefano.cecere@krur.com>
2022-12-07 19:09:46 +0100

    minigame Sequence

---
+++ Assets/_minigames/game_sequence/Scripts/MyPiece.cs
@@ -0,0 +1,18 @@
+﻿using Ieedo.games;
+using UnityEngine;
+
+namespace Ieedo.games.sequence
+{
+    public class MyPiece : Piece
+    {
+        void Awake()
+        {
+            SetAction(Clicked);
+        }
+
+        private void Clicked(Piece piece)
+        {
+            Debug.Log("CLICKED piece with Number " + piece.Cell.X + " - " + piece.Cell.Y);
+        }
+    }
+}
