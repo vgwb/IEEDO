9bb4cb6cec28ce71f2b258c51e07ce9ca17623e6
Michele <cat.piro@gmail.com>
2022-12-07 14:46:48 +0100

    Score does not use localization anymore

--- Assets/_core/_scripts/UI/ScoreManager.cs
+++ Assets/_core/_scripts/UI/ScoreManager.cs
@@ -14,31 +14,14 @@ namespace Ieedo
         // @note: used by Localization
         public string CurrentScore => $"{Statics.Data.Profile.CurrentScore.ToString(),5}";
 
-        void Start()
-        {
-            if (CurrentScoreText != null)
-            {
-                CurrentScoreText.Key = new LocalizedString("UI", "top_score");
-                CurrentScoreText.Key.Arguments = new List<object> { this };
-                CurrentScoreText.Key.StringChanged += v =>
-                {
-                    CurrentScoreText.text = v;
-                };
-            }
-        }
-
         public void AddScore(int value)
         {
             SoundManager.I.PlaySfx(SfxEnum.score);
             Statics.Data.Profile.CurrentScore += value;
             CurrentScoreText.transform.localScaleTransition(Vector3.one * 1.5f, 0.25f).JoinDelayTransition(0.25f).localScaleTransition(Vector3.one, 0.25f);
-            CurrentScoreText.Key.RefreshString();
+            CurrentScoreText.Text = CurrentScore;
             Statics.Data.SaveProfile();
         }
 
-        public void RefreshString()
-        {
-            CurrentScoreText?.Key.RefreshString();
-        }
     }
 }
