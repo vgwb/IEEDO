b4dcefa38be33924f522ec1f19750c3c12487c4b
Stefano Cecere <stefano.cecere@krur.com>
2022-12-21 10:57:21 +0100

    add skip invisible button (not working yet)

---
+++ Assets/_scripts/Utilities/SkipButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ieedo.Utilities
+{
+    public class SkipButton : MonoBehaviour
+    {
+
+        public void OnTapMe()
+        {
+            Debug.Log("TAPPING");
+        }
+    }
+}
