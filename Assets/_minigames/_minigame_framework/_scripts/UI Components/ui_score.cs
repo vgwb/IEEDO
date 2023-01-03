29ba7ed8691e56727a944e115ef73f0a2dd1b592
Stefano Cecere <stefano.cecere@krur.com>
2023-01-03 17:24:11 +0100

    minigames

--- Assets/_minigames/_minigame_framework/_scripts/UI Components/ui_score.cs
+++ Assets/_minigames/_minigame_framework/_scripts/UI Components/ui_score.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 namespace minigame
 {
-    public class timer : MonoBehaviour
+    public class ui_score : MonoBehaviour
     {
+        // Start is called before the first frame update
         void Start()
         {
 
         }
 
+        // Update is called once per frame
         void Update()
         {
 
---
+++ Assets/_minigames/_minigame_framework/_scripts/UI Components/ui_timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace minigame
+{
+    public class ui_timer : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
