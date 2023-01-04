159bf2b60a6382e7c95840c38e64491ecbe47506
Michele <cat.piro@gmail.com>
2023-01-04 15:12:09 +0100

    Refactored activity continue screen

--- Assets/_scripts/UI/UIActivityContinueScreen.cs
+++ Assets/_scripts/UI/UIActivityContinueScreen.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
 
 namespace Ieedo
 {
@@ -21,29 +23,48 @@ namespace Ieedo
 
         public IEnumerator ShowResult(ActivityResult result)
         {
-            //Title.Text.Key = new LocalizedString("UI", "activity_result_title");
+            var activityDef = Statics.ActivityFlow.CurrentActivity;
+            var activityData = Statics.Data.Profile.Activities.GetActivityData(Statics.ActivityFlow.CurrentActivity.ID);
 
             switch (Statics.ActivityFlow.CurrentActivity.Type)
             {
                 case ActivityType.Daily:
                     Title.Text.Key = new LocalizedString("UI", $"activity_result_daily_complete");
-                    Content.Text.Key =  new LocalizedString("UI", "activity_result_write_everyday");
+                    Content.Text.Key =  new LocalizedString("UI", $"{activityDef.SceneName}_end");
                     break;
                 case ActivityType.Game:
-                    Title.Text.Key = new LocalizedString("UI", $"activity_result_game_{result.Result.ToString().ToLower()}");
-                    Content.Text.Key =  new LocalizedString("UI", "activity_result_play_everyday");
-                    break;
+                    switch (activityDef.ScoreType)
+                    {
+                        case ScoreType.Highscore:
+                            Title.Text.Key = new LocalizedString("UI", $"activity_result_game_over");
+                            Content.Text.Key =  new LocalizedString("UI", $"{activityDef.SceneName}_end");
+                            NextLevelButton.Key = new LocalizedString("UI", "play_again");
+                            break;
+                        case ScoreType.LevelReached:
+                            Title.Text.Key = new LocalizedString("UI", $"activity_result_game_{result.Result.ToString().ToLower()}");
+                            Content.Text.Key = new LocalizedString("UI", "activity_result_play_everyday");
+                            NextLevelButton.Key = new LocalizedString("UI", "play_next_level");
+                            break;
+                    }
+                   break;
             }
 
             NextLevelButton.gameObject.SetActive(Statics.ActivityFlow.CurrentActivity.Type == ActivityType.Game);
             QuitButton.gameObject.SetActive(Statics.ActivityFlow.CurrentActivity.Type == ActivityType.Daily);
 
-            var activityData = Statics.Data.Profile.Activities.GetActivityData(Statics.ActivityFlow.CurrentActivity.ID);
             ScorePivot.SetActive(Statics.ActivityFlow.CurrentActivity.ScoreType == ScoreType.Highscore);
-            CurrentScore.Text.Text = "New Score: " + result.Score.ToString();
-            HighScore.Text.Text = "Highscore: " + activityData.MaxScore.ToString();
 
-            SetupButton(NextLevelButton, Continue);
+            var scoreLoc = new LocalizedString("UI", $"activity_result_score");
+            scoreLoc.Arguments = new List<object> { result.Score };
+            scoreLoc.Add("Score", new IntVariable{Value = result.Score});
+            CurrentScore.Text.Key = scoreLoc;
+
+            var highscoreLoc = new LocalizedString("UI", $"activity_result_highscore");
+            highscoreLoc.Arguments = new List<object> { activityData.MaxScore };
+            highscoreLoc.Add("HighScore", new IntVariable{Value = activityData.MaxScore});
+            HighScore.Text.Key = highscoreLoc;
+
+            SetupButton(NextLevelButton, () => Continue());
             SetupButton(QuitButton, Quit);
             yield return OpenCO();
         }
