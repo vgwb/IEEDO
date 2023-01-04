3e4ba078bc1a5f4eeeb335390603436e454e29ca
Stefano Cecere <stefano.cecere@krur.com>
2023-01-04 13:12:19 +0100

    activity skills

---
+++ Assets/_scripts/Data/ActivitySkills.cs
@@ -0,0 +1,43 @@
+namespace Ieedo
+{
+    public enum ActivityCategory
+    {
+        None = 0,
+        category_attention = 1,
+        category_emotions = 2,
+        category_flexibility = 3,
+        category_math = 4,
+        category_memory = 5,
+        category_problem_solving = 6,
+        category_speed = 7,
+        category_word = 8,
+    }
+
+    public enum ActivitySkills
+    {
+        None = 0,
+        skill_emotions_sense = 1,
+        skill_emotions_inclusiveness = 2,
+        skill_attention_divided = 3,
+        skill_attention_selective = 4,
+        skill_attention_task_switching = 5,
+        skill_coordination_timing = 6,
+        skill_field_of_view = 7,
+        skill_logical_reasoning = 8,
+        skill_math_numerical_calculation = 9,
+        skill_math_numerical_estimation = 10,
+        skill_math_probabilistic_reasoning = 11,
+        skill_memory_working = 12,
+        skill_planning = 13,
+        skill_processing_speed = 14,
+        skill_proportional_reasoning = 15,
+        skill_reading_comprehension = 16,
+        skill_response_inhibition = 17,
+        skill_spatial_orientation = 18,
+        skill_spatial_reasoning = 19,
+        skill_spatial_recall = 20,
+        skill_visualization = 21,
+        skill_vocabulary_proficiency = 22,
+        skill_word_verbal_fluency = 23
+    }
+}
