99f8131132c8518d21a54b06b57e248d265c1453
Michele <cat.piro@gmail.com>
2023-01-09 17:07:15 +0100

    Fix - [Tutorial] No finger appears over the Create New Card the frist time you reach the To Do List #161

--- Assets/_scripts/managers/TutorialManager.cs
+++ Assets/_scripts/managers/TutorialManager.cs
@@ -6,11 +6,22 @@ namespace Ieedo
     {
         public GameObject TutorialArrow;
 
+        private GameObject targetGo;
+
         public void ShowTutorialArrowOn(GameObject go, float orientation = 0f)
         {
             TutorialArrow.SetActive(true);
+            targetGo = go;
             TutorialArrow.transform.position = go.transform.position;
             TutorialArrow.transform.localEulerAngles = new Vector3(0, 0, orientation);
+        }
+
+        public void Update()
+        {
+            if (TutorialArrow.activeSelf)
+            {
+                TutorialArrow.transform.position = targetGo.transform.position;
+            }
         }
 
         public void HideTutorialArrow()
