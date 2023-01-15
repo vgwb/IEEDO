9782bfff9a673c1ad7d1d43244f0e11f255b7519
Stefano Cecere <stefano.cecere@krur.com>
2023-01-16 00:22:42 +0100

    vgwb framework

---
+++ Assets/_vgwb_framework/Singletons/SingletonScriptableObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace vgwb.framework
+{
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T instance = null;
+        public static T I
+        {
+            get {
+                if (!instance)
+                    instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+#if UNITY_EDITOR
+                if (!instance) {
+                    string[] configsGUIDs = UnityEditor.AssetDatabase.FindAssets("t:" + typeof(T).Name);
+                    if (configsGUIDs.Length > 0) {
+                        instance = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(UnityEditor.AssetDatabase.GUIDToAssetPath(configsGUIDs[0]));
+                    }
+                }
+#endif
+                return instance;
+            }
+        }
+
+#if UNITY_EDITOR
+        public void OnEnable()
+        {
+            AddToPreloaded();
+        }
+
+        private void AddToPreloaded()
+        {
+            if (I != null) {
+                var preloadedAssets = PlayerSettings.GetPreloadedAssets().ToList();
+                if (!preloadedAssets.Contains(I)) {
+                    preloadedAssets.Add(I);
+                    PlayerSettings.SetPreloadedAssets(preloadedAssets.ToArray());
+                }
+            }
+        }
+#endif
+    }
+}
