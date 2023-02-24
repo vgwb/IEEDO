d95b77828d9db7544771ff218710f8db99bf1eb8
Stefano Cecere <stefano.cecere@krur.com>
2023-02-24 18:18:17 +0100

    fix local notifications

--- Assets/_scripts/Services/NotificationService.cs
+++ Assets/_scripts/Services/NotificationService.cs
@@ -10,96 +10,79 @@ namespace Ieedo
         private bool NotificationsEnabled => Statics.App.ApplicationConfig.NotificationsEnabled;
 
         public GameNotificationsManager NotificationsManager;
-        public const string ChannelId = "game_remind";
+        public const string ReminderChannelId = "reminder_channel1";
+        private readonly string smallIconName = "icon_0";
+        private readonly string largeIconName = "icon_1";
         protected int playReminderHour = 7;
 
         private IEnumerator Start()
         {
             if (NotificationsEnabled)
             {
-                Debug.Log("NotificationService Init");
-                var channel = new GameNotificationChannel(ChannelId, "Reminder Channel", "Reminder notifications");
-                return NotificationsManager.Initialize(channel);
+                //                Debug.Log("NotificationService Init");
+                var channel = new GameNotificationChannel(ReminderChannelId, "Reminder Channel", "Reminder notifications");
+                yield return NotificationsManager.Initialize(channel);
+                ScheduleRemindNotification();
             }
             else
             {
-                return null;
+                yield return null;
             }
         }
 
-        #region main
-        /// <summary>
-        /// automatically call everything to setup Notifications at AppSuspended
-        /// </summary>
-        public void AppSuspended()
+        private void OnApplicationFocus(bool hasFocus)
         {
-            if (NotificationsEnabled)
+            if (hasFocus)
             {
-                PrepareNextLocalNotification();
+                if (NotificationsEnabled)
+                {
+                }
             }
         }
 
-        /// <summary>
-        /// automatically restore all Notifications at AppResumed
-        /// </summary>
-        public void AppResumed()
+        private void ScheduleRemindNotification()
         {
-            // if (!NotificationsManager.Initialized)
-            // {
-            //     Init();
-            // }
-            NotificationsManager.DismissAllNotifications();
-        }
-        #endregion
-
-        private void PrepareNextLocalNotification()
-        {
-            //DeleteAllLocalNotifications();
-            Debug.Log("Next Local Notifications prepared");
             ScheduleNotification(
                 GetDateTimeTomorrow(),
                 "IEEDO App",
                 LocString.FromStr("UI/notifications_remind_24h").GetLocalizedString()
             );
-
-            //NotificationManager.ScheduleSimpleWithAppIcon(
-            //    TimeSpan.FromSeconds(60),
-            //    "Antura and the Letters",
-            //    "Test notification after closing the app [60 seconds]",
-            //    Color.blue
-            //);
+            //            Debug.Log("Next Remind Notification prepared");
         }
-
-        #region direct plugins methods
 
         public void ScheduleNotification(DateTime deliveryTime, string title, string message)
         {
             IGameNotification notification = NotificationsManager.CreateNotification();
-
+            if (notification == null)
+            {
+                return;
+            }
             notification.Title = title;
             notification.Body = message;
-            notification.Group = ChannelId;
+            notification.Group = ReminderChannelId;
             notification.DeliveryTime = deliveryTime;
-            notification.LargeIcon = "icon_antura";
-            NotificationsManager.ScheduleNotification(notification);
+            notification.SmallIcon = smallIconName;
+            notification.LargeIcon = largeIconName;
 
+            PendingNotification notificationToDisplay = NotificationsManager.ScheduleNotification(notification);
+            notificationToDisplay.Reschedule = false;
             Debug.Log("ScheduleNotification - " + deliveryTime);
         }
 
         public void DeleteAllLocalNotifications()
         {
+            NotificationsManager.Platform.CancelAllScheduledNotifications();
 
         }
-        #endregion
 
         #region time utilities
         private DateTime GetDateTimeTomorrow()
         {
             DateTime deliveryTime = DateTime.Now.ToLocalTime().AddDays(1);
+            //DateTime deliveryTime = DateTime.Now.ToLocalTime().AddMinutes(2);
             deliveryTime = new DateTime(deliveryTime.Year, deliveryTime.Month, deliveryTime.Day, playReminderHour, 0, 0,
                 DateTimeKind.Local);
             return deliveryTime;
-            //return DateTime.Now.AddHours(20);
         }
 
         private DateTime GetDateTimeInMinutes(int minutes)
